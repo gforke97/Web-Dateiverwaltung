12154d115fb01cd10c26de884fcbd34f5f271bb4
webserver <test@test.de>
2018-03-07 09:18:04 +0100

    unwichtiger Commit

--- php/index.php
+++ php/index.php
@@ -141,6 +141,10 @@ $_SESSION['ip'] = "192.168.0.16";
 $_SESSION['user'] = "test";
 $_SESSION['pass'] = "test";
 $_SESSION['aktordner'] = "";
+#$_SESSION['ip'] = "ftp.dlptest.com";
+#$_SESSION['user'] = "dlpuser@dlptest.com";
+#$_SESSION['pass'] = "eiTqR7EMZD5zy7M";
+#$_SESSION['aktordner'] = "";
 
 ?>
 
@@ -278,7 +282,7 @@ $_SESSION['aktordner'] = "";
 	</script>
 
 
-
+<div>Icons made by <a href="https://www.flaticon.com/authors/ocha" title="OCHA">OCHA</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
   </body>
 
 </html>
