4f7218351675095beb825990a2d9250360d4369f
Florian <florianlehmkuhl@yahoo.de>
2018-03-08 11:09:10 +0100

    Default Index.html umbenennen.

--- install/install_dateiverwaltung.sh
+++ install/install_dateiverwaltung.sh
@@ -60,6 +60,7 @@ continue through installations
 
 wget -O /tmp/master.zip https://github.com/gforke97/Web-Dateiverwaltung/archive/master.zip
 unzip -o /tmp/master.zip -d /tmp/web-dateiverwaltung/
+mv -f /var/www/html/index.html /var/www/html/index.html.old
 mv -f /tmp/web-dateiverwaltung/*/* /var/www/html/
 rm -rf /tmp/master.zip
 rm -rf /tmp/web-dateiverwaltung/
