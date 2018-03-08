4e185c837fe4ea5a88ed5069ea0c90a0d1436500
severus157 <severus157@github.com>
2018-03-08 10:41:56 +0100

    test

--- utils/showdirectorycontent.php
+++ utils/showdirectorycontent.php
@@ -56,7 +56,7 @@ echo "<td>Ordner</td>";
 echo "<td>$ordner[day]. $ordner[month]</td>";
 $vollerpfad = $aktordner . DIRECTORY_SEPARATOR . $ordner[name] . DIRECTORY_SEPARATOR;
 echo "<td class=\"button_row\"><button class=\"btn image_button\" type=\"button\" onclick=\"deletedirectory('$vollerpfad');\"><img class=\"img\" id=\"thumbnail-pic\" src=\"img/trashbin.png\" alt=\"Trash Bin\"></button>";
-echo "<button class=\"btn image_button\" type=\"button\" onclick=\"renamefile('$ordner[name]');\">Umbenennen</button></td>";
+echo "<button class=\"btn image_button\" type=\"button\" onclick=\"renamefile('$ordner[name]');\"><img class=\"thumbnail-pic\" src=\"img/rename.png\" alt=\"Rename\"></button></td>";
 echo "</tr>";
 }
 
