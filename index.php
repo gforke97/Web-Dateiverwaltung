76e4b24d2a41d72254dd2b0e38448f56a6ec8247
webserver <test@test.de>
2018-03-19 14:05:46 +0100

    Copied JS to external file.

--- index.php
+++ index.php
@@ -11,121 +11,11 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+	<script src="main.js"></script>
+	
     <!--Main CSS-->
     <link rel="stylesheet" type="text/css" href="main.css">
-
-	<script>
-	function showfiles(session) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("fileslist"+session).innerHTML = this.responseText;
-        }
-    };
-    var url = "utils/showdirectorycontent.php?session=" + session;
-	xmlhttp.open("GET", url, false);
-    xmlhttp.send();
-	}
-
-
-	function changedirectory(session, str) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp = new XMLHttpRequest();
-		
-	var url = "utils/changedirectory.php?session=" + session + "&ordner=" + str;
-	xmlhttp.open("GET",url, false);
-    xmlhttp.send();
-
-	showfiles(session);
-	}
-
-    function downloadfile(session, str) {
 	
-	var url = "utils/downloadfile.php?session=" + session + "&datei=" + str;
-
-	window.open(url);
-	}
-
-
-	function deletefile(session, str) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp = new XMLHttpRequest();
-		
-    if (confirm('Möchten Sie die Datei wirklich löschen?')) {
-	var url = "utils/deletefile.php?session=" + session + "&vollerpfad=" + str;
-    xmlhttp.open("GET",url, false);
-    xmlhttp.send();
-
-    showfiles(session);
-    } else {
-    // nichts
-    }
-	}
-
-
-    function renamefile(session, str) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp = new XMLHttpRequest();
-
-	xmlhttp.onload = function () {
-	if (xmlhttp.status === 200) {
-    // Datei umbenannt.
-	showfiles(session);
-	} else {
-    alert('Fehler beim Umbenennen!');
-	}
-	};
-
-	var neuername = prompt("Bitte einen neuen Dateinamen angeben.", str);
-
-	if (neuername != null) {
-	var url = "utils/renamefile.php?session=" + session + "&altername=" + str + "&neuername=" + neuername;
-    xmlhttp.open("GET", url, false);
-    xmlhttp.send();
-	}
-    }
-
-
-	function deletedirectory(session, str) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp = new XMLHttpRequest();
-
-    if (confirm('Möchten Sie den Ordner mit allen Dateien und Unterordnern wirklich löschen?')) {
-    var url = "utils/deletedirectory.php?session=" + session + "&vollerpfad=" + str;
-    xmlhttp.open("GET",url, false);
-    xmlhttp.send();
-
-    showfiles(session);
-    } else {
-    // nichts
-	}
-	}
-	
-	function createdirectory(session) {
-	xmlhttp = new XMLHttpRequest();
-
-	xmlhttp.onload = function () {
-	if (xmlhttp.status === 200) {
-    //Ordner erstellt.
-	showfiles(session);
-	} else {
-    alert('Fehler beim Erstellen des Ordners!');
-	}
-	};
-
-	var ordnername = prompt("Bitte einen neuen Ordnernamen angeben.");
-
-	if (ordnername != null) {
-	var url = "utils/createdirectory.php?session=" + session + "&ordnername=" + ordnername;
-    xmlhttp.open("GET", url, false);
-    xmlhttp.send();
-	}
-	}
-
-</script>
   </head>
 
   <body>
@@ -242,7 +132,6 @@ $_SESSION['aktordner2'] = "";
 
 	<script>
 	dateihochladenhandler();
-	ordnererstellenhandler();
 
 	function dateihochladenhandler() {
 	var form = document.getElementById("file-form");
---
+++ main.js
@@ -0,0 +1,108 @@
+	function showfiles(session) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("fileslist"+session).innerHTML = this.responseText;
+        }
+    };
+    var url = "utils/showdirectorycontent.php?session=" + session;
+	xmlhttp.open("GET", url, false);
+    xmlhttp.send();
+	}
+
+
+	function changedirectory(session, str) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+		
+	var url = "utils/changedirectory.php?session=" + session + "&ordner=" + str;
+	xmlhttp.open("GET",url, false);
+    xmlhttp.send();
+
+	showfiles(session);
+	}
+
+    function downloadfile(session, str) {
+	
+	var url = "utils/downloadfile.php?session=" + session + "&datei=" + str;
+
+	window.open(url);
+	}
+
+
+	function deletefile(session, str) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+		
+    if (confirm('Möchten Sie die Datei wirklich löschen?')) {
+	var url = "utils/deletefile.php?session=" + session + "&vollerpfad=" + str;
+    xmlhttp.open("GET",url, false);
+    xmlhttp.send();
+
+    showfiles(session);
+    } else {
+    // nichts
+    }
+	}
+
+
+    function renamefile(session, str) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+
+	xmlhttp.onload = function () {
+	if (xmlhttp.status === 200) {
+    // Datei umbenannt.
+	showfiles(session);
+	} else {
+    alert('Fehler beim Umbenennen!');
+	}
+	};
+
+	var neuername = prompt("Bitte einen neuen Dateinamen angeben.", str);
+
+	if (neuername != null) {
+	var url = "utils/renamefile.php?session=" + session + "&altername=" + str + "&neuername=" + neuername;
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send();
+	}
+    }
+
+
+	function deletedirectory(session, str) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+
+    if (confirm('Möchten Sie den Ordner mit allen Dateien und Unterordnern wirklich löschen?')) {
+    var url = "utils/deletedirectory.php?session=" + session + "&vollerpfad=" + str;
+    xmlhttp.open("GET",url, false);
+    xmlhttp.send();
+
+    showfiles(session);
+    } else {
+    // nichts
+	}
+	}
+	
+	function createdirectory(session) {
+	xmlhttp = new XMLHttpRequest();
+
+	xmlhttp.onload = function () {
+	if (xmlhttp.status === 200) {
+    //Ordner erstellt.
+	showfiles(session);
+	} else {
+    alert('Fehler beim Erstellen des Ordners!');
+	}
+	};
+
+	var ordnername = prompt("Bitte einen neuen Ordnernamen angeben.");
+
+	if (ordnername != null) {
+	var url = "utils/createdirectory.php?session=" + session + "&ordnername=" + ordnername;
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send();
+	}
+	}	
